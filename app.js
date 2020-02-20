8d4848a41efb50e29c5b2c2aa94569ff3d94817d
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-02-20 12:15:35 +0200

    the product function

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function sum(a, b) { //eslint-disable-line
     var message = `The sum of ${a} and ${b} is ${theSum}.`;
     return [theSum, message];
 }
-testSum(4,7);
+testSum();
 
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
@@ -31,11 +31,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+var theProduct=a*b;
+//"The product of 5 and 9 is 45."
+var message = `The product of ${a} and ${b} is ${theProduct}.`;
+return [theProduct , message ];
 
 }
-
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply();
+ //Here is the test for multiply(); uncomment it to run it
+ //testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
